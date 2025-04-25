8e5c30ebe7f7c750e327ccfa24d72967e9e3fbc6
Alexis Graves <avgraves1@gmail.com>
2025-04-25 10:43:42 -0400

    counter

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,11 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { CounterComponent } from "./counter/counter.component";
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  imports: [CounterComponent],
+  styleUrl: './app.component.scss',
 })
 export class AppComponent {
-  title = 'counter';
 }
